94cde96ccdca2d71d3697f229b85c0505f71a541
DASH-SIMANTA <cse.simanta.dash@gmail.com>
2025-01-23 21:18:13 +0600

    update
    sort asc dsc

---
+++ my-javascript/sort.js
@@ -0,0 +1,13 @@
+const persons=['rakib','nokib', 'sakib', 'akib','dakib'];
+const sortPersons=persons.sort();
+console.log(sortPersons);
+/* sort>>>
+ Ascending>>smaller to lager
+ descending>>larger to smaller
+ */
+
+const numbers= [4,7,12,43,6,1];
+// const number_asc = numbers.sort();//not working
+const number_asc=numbers.sort(function(a,b) {return a-b});
+const number_dsc=numbers.sort(function(a,b) {return b-a});
+console.log(number_asc);
