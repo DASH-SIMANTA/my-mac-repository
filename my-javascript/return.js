421cc0e9c42c6cda3e92177a5f4f9b66bafca087
DASH-SIMANTA <cse.simanta.dash@gmail.com>
2025-01-24 01:32:49 +0600

    update
    function return and set return to a variable

---
+++ my-javascript/return.js
@@ -0,0 +1,15 @@
+//20_4 function return and set return value to a variable
+function tenTimes(number){
+    const result = number *10;
+    return result;
+}
+
+function cutHalf(){
+    const half = number / 2;
+    return half;
+}
+tenTimes(5);
+
+const output = tenTimes(3);
+
+console.log('output', output);
