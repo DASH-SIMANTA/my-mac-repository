768c15ae9cb97ae3c3f4092a11638ec0de9f9d1f
DASH-SIMANTA <cse.simanta.dash@gmail.com>
2025-01-26 00:36:42 +0600

    update
    Average of odd-numbers

---
+++ my-javascript/js-problems-part1/odd-average.js
@@ -0,0 +1,26 @@
+/**
+ * function takes an array as parameter
+ * Give me the average of the odd number of the array
+ */
+
+function oddAverage(numbers){
+    const odds=[];
+    for(const number of numbers){
+        if(number % 2 ===1){
+            // console.log(number);
+            odds.push(number);
+        }
+    }
+    // console.log(odds);
+    let sum = 0;
+    for(const number of odds){
+        sum = sum+number;
+    }
+    const count =odds.length;
+    console.log(sum, count);
+    const avg = sum / count;
+    return avg;
+}
+const numbers = [42,13,56,73,37,98];
+const avg =oddAverage(numbers);
+console.log('Average of the odd number is: ',avg);
