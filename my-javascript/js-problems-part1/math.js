e0f3acdb2517d3087607b37f569db4b7f47580aa
DASH-SIMANTA <cse.simanta.dash@gmail.com>
2025-01-27 00:44:53 +0600

    update
    math

---
+++ my-javascript/js-problems-part1/math.js
@@ -0,0 +1,22 @@
+// const Math ={
+//     min: function min(num1, num2){
+//         return num2;
+//     }
+// }
+// console.log(Math.min);
+// const min= Math.min;
+
+const min= Math.min(12,543,24,67,47,39,432,-99);
+console.log(min);
+const max = Math.max (34,998,76,554,24,56,6,33,-99);
+console.log(max);
+
+console.log(Math.PI);
+console.log(Math.abs(10-5));
+console.log(Math.round(12.33));
+console.log(Math.floor(12.33));
+console.log(Math.ceil(12.33));
+console.log(Math.random());
+console.log(Math.random()*10);
+const rand = Math.round(Math.random()*10);
+console.log(rand);
