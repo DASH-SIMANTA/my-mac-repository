9dc9f8f45e6a14b1ec36f3fc139845ce0a8073c8
DASH-SIMANTA <cse.simanta.dash@gmail.com>
2025-02-03 20:50:12 +0600

    update
    types of event handler 26-3

--- JS-DOM-EVENTS/js/events.js
+++ JS-DOM-EVENTS/js/events.js
@@ -1,7 +1,10 @@
 console.log('This is seperate js file');
 //option-1: directly set on the html element
+//<button onclick="console.log(65)">Another click</button>
 
 //option-2 : add onclick function
+//IMPORTANT: we will use this
+//<button onclick="makeRed()">Make Red</button>
 function makeRed(){
     document.body.style.backgroundColor='red';
 }
@@ -20,3 +23,23 @@ const purpleButton = document.getElementById('make-purple');
     purpleButton.onclick = function makePurple(){
         document.body.style.backgroundColor = 'purple';
     }
+
+
+//option-4:
+const pinButton = document.getElementById('make-pink');
+        pinButton.addEventListener('click', makePink);
+        function makePink(){
+            document.body.style.backgroundColor='pink';
+        }
+//option-4: Another
+const makeGreenButton = document.getElementById('make-green');
+        makeGreenButton.addEventListener('click', function makeGreen(){
+            document.body.style.backgroundColor ='green';
+        })
+//option-4 Final
+//IMPORTANT: WE WILL USE THIS SOMETIMES
+  // document.getElementById('make-goldenrod').addEventListener?(function(){})
+  document.getElementById('make-goldenrod').addEventListener('click',function(){
+    document.body.style.background ='goldenrod';
+
+})
