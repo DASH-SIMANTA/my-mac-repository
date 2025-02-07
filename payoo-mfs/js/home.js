0dba7fa24fdf3ac98ad7f4f61a9b0c62918333d2
DASH-SIMANTA <cse.simanta.dash@gmail.com>
2025-02-08 00:38:43 +0600

    update
    verify pin & add money to the balance

--- payoo-mfs/js/home.js
+++ payoo-mfs/js/home.js
@@ -9,11 +9,28 @@ document.getElementById('btn-add-money').addEventListener('click',function(event
     console.log('money add button clicked');
     //s-2
     const addAmountInput = document.getElementById('add-money-input').value;
-    console.log(addAmountInput);
+    console.log(typeof addAmountInput);
     const pinInput = document.getElementById('pin-input').value;
     //get the pin number provided
     console.log(pinInput);
-    
+
+    //add validation
+    if(pinInput==='1234'){
+        console.log('Adding money to your account');
+        //s-4: get the current balance
+        const balance =document.getElementById('account-balance').innerText;
+        console.log(balance);
+        //s-5:add addMoneyInput with balance
+        const newBalance =parseFloat(balance)  + parseFloat(addAmountInput);
+        console.log(newBalance);
+        //s-6:
+        //update the balance in the UI/DOM
+        document.getElementById('account-balance').innerText = newBalance;
+
+    }
+    else{
+        alert('Failed to add money! Please try again later');
+    }
 
 
 })
