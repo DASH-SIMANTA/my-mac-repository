d06ea0beaa1c8f74f3493fecec75c8528a5c5dbc
DASH-SIMANTA <cse.simanta.dash@gmail.com>
2025-02-08 00:02:14 +0600

    update
    handle add money event on the button click

--- payoo-mfs/js/home.js
+++ payoo-mfs/js/home.js
@@ -1,1 +1,19 @@
-console.log('money added');
+//add money to the account
+//step-1: add an event handler to the add money button inside the form
+//prevent page reload after form submit
+// step-2: get money to be added the account balance
+
+document.getElementById('btn-add-money').addEventListener('click',function(event){
+    //prevent page reload after form submit
+    event.preventDefault();
+    console.log('money add button clicked');
+    //s-2
+    const addAmountInput = document.getElementById('add-money-input').value;
+    console.log(addAmountInput);
+    const pinInput = document.getElementById('pin-input').value;
+    //get the pin number provided
+    console.log(pinInput);
+    
+
+
+})
