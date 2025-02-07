2257b1d3b15e000f8eb399ddb148a5fb81486e31
DASH-SIMANTA <cse.simanta.dash@gmail.com>
2025-02-07 21:25:04 +0600

    update
    handle login and redirect to the home page

--- payoo-mfs/js/login.js
+++ payoo-mfs/js/login.js
@@ -4,8 +4,18 @@ document.getElementById('button-login').addEventListener('click',function(event)
 //step-2: prevent default behavior (prevent reloading browser)/to stop reloading browser
     event.preventDefault();
     //step-3: get the phone number
-    const phoneNumber = document.getElementById('phone-number');
-    console.log(phoneNumber);
+    const phoneNumber = document.getElementById('phone-number').value;
+    const pinNumber = document.getElementById('pin-number').value;
+    console.log(phoneNumber,pinNumber);
+    //step-4: validate
+    //bad way to validate
+    if(phoneNumber=== '5' && pinNumber==='1234'){
+        console.log('you are logged in');
+        window.location.href ='/home.html'
+    }
+    else{
+        alert('wrong phone number or pin');
+    }
 
 
 })
