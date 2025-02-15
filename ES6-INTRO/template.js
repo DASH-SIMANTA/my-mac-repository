8803b5d24c62ab991e3e45724f065515dcd5ece9
DASH-SIMANTA <cse.simanta.dash@gmail.com>
2025-02-15 19:50:52 +0600

    update
    es6

---
+++ ES6-INTRO/template.js
@@ -0,0 +1,34 @@
+const first = 'simanta'
+const last = 'dash'
+const greet = 'potas potas';
+
+const name = first +' ' + " " + greet;
+console.log(name);
+
+const a = 10;
+const b = 20;
+const result = 'the sum of '+ a + ' and ' + b + ' is ' + (a + b);
+console.log(result);
+const numbers = [24,23,45,43,21,11];
+const student = {name: 'simanta dash', age: 30}  
+
+const math = `the sum of ${a} and ${b} is ${a+b}`
+const studentDetails = `the sum of ${numbers[0]} and ${student.age} is ${a+b}`
+console.log(math);
+console.log(studentDetails);
+
+const email  = 'hi jhon \n' +
+'cena cena lage \n ' +
+'bosonto ashe geche';
+
+console.log(email);
+
+
+const challenge = `ekhon february mash 
+sobai prem kore 
+ami code kori`
+console.log(challenge);
+
+
+console.log(studentDetails);
+
---
+++ ES6-INTRO/var-let-const.js
@@ -0,0 +1,38 @@
+//var : no reason to use bar
+//let : allow it to reassign
+//const : do not allow it to reassign 
+
+const  money = 25;
+const rich = money +25;
+console.log(rich);
+
+ let count = 0;
+ count = count + 10;
+ console.log(count);
+
+
+
+ const numbers = [23, 44, 54, 33, 22];
+ numbers[1] = 55;
+ numbers.push(12,343,49);
+ console.log(numbers);
+
+//object
+ const student = {
+    name:'moyna pakhi',
+    class: 12
+ }
+
+
+ student.name = 'kokil konthi';
+ console.log(student);
+
+
+
+ //loop
+let sum = 0;
+ for(let i= 0; i<10; i++){
+    
+    sum= sum + i;
+
+ }
