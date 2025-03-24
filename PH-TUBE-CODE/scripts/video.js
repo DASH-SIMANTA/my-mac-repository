d98a44a949776fd4011a9aba68660a77ba149922
DASH-SIMANTA <cse.simanta.dash@gmail.com>
2025-03-25 02:42:14 +0600

    update
    videos

---
+++ PH-TUBE-CODE/scripts/video.js
@@ -0,0 +1,38 @@
+// console.log('script added');
+
+//1. Fetch, Load and Show Categories on html
+
+//create loadCategories
+const loadCategories = () =>{
+    // fetch the data
+    fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
+    .then(res =>res.json())
+    .then(data =>DisplayCategories(data.categories))
+    .catch(error=> console.log(error))
+
+}
+
+//Create DisplayCategories
+
+// {category_id: "1001", category: "Music"}
+
+const DisplayCategories = (categories) =>{
+    const categoryContainer = document.getElementById('categories');
+
+    // console.log(data)
+    categories.forEach((item)=>{
+        console.log(item);
+        //create a button
+        const button =document.createElement('button');
+        button.classList = 'btn';
+        button.innerText = item.category;
+
+        //add button to category container
+        categoryContainer.append(button);
+
+    })
+};
+
+
+loadCategories();
+// DisplayCategories();
