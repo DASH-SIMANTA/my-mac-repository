1051a7e64bf3ed214e23d6d8d42ec3f4e9c8aa49
DASH-SIMANTA <cse.simanta.dash@gmail.com>
2025-04-20 21:08:56 +0600

    update
    notation

---
+++ javaScriptFundamentalThatYouMustKnow/notation.js
@@ -0,0 +1,26 @@
+const person ={
+    name: 'lal nil',
+    profession: 'Traffic Surjon',
+    age: 34,
+    address: 'shahabag',
+    25: 'ha ha ha',
+    'son-name': 'sunny'
+}
+
+//dot notation
+
+const prof1 = person.profession;
+
+//bracket notation
+
+const prof2 = person['profession'];
+const pName ='profession';
+const prof3 = person[pName];
+// console.log(prof2);
+// console.log(prof3);
+const season = person[25];
+
+const son = person['son-name'];
+
+
+
