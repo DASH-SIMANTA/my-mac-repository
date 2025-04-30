81ab0934bb9df4639885a049e279c46c6856b6f3
DASH-SIMANTA <cse.simanta.dash@gmail.com>
2025-05-01 01:27:08 +0600

    update
    recap

---
+++ react-core-concept3/src/Posts.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react"
+import Post from "./Post";
+
+
+export default function Posts(){
+    const [posts,setPosts] =useState([]);
+    useEffect(()=>{
+        fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(res=>res.json())
+        .then(data=>setPosts(data))
+    },[])
+    
+    return (
+        <div className="post">
+            <h3>post:{posts.length} </h3>
+            {
+                posts.map(post => <Post post={post}></Post>)
+            }
+            
+           
+            
+        </div>
+    )
+}
+
+/**
+ * 1. create a state to store data
+ * 2. create use effect with no dependency
+ */
