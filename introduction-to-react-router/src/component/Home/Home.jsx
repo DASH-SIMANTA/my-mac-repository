9cdd2a7e797ecd9888848371726e2469a133724e
DASH-SIMANTA <cse.simanta.dash@gmail.com>
2025-07-04 05:36:47 +0600

    update
    Active route,Loading Spinner, Uselocation, useNavigation

--- introduction-to-react-router/src/component/Home/Home.jsx
+++ introduction-to-react-router/src/component/Home/Home.jsx
@@ -1,13 +1,23 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation, useNavigation } from "react-router-dom";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 
 
 const Home = () => {
+    const navigation = useNavigation();
+    const location = useLocation();
+    console.log('Current location:', location);
+    // The useLocation hook is used to access the current location object
+    // It provides information about the current URL, including the pathname,
+    // search parameters, and hash fragment.        
     return (
         <div>
             <Header></Header>
-            <Outlet></Outlet>
+            {
+                navigation.state === 'loading' ? 
+                <p>Loading...</p>:<Outlet></Outlet>
+            }
+            
             <Footer></Footer>
             
         </div>
