5754a7e3ff3c2477cf35f213edab67a56aab7c53
DASH-SIMANTA <cse.simanta.dash@gmail.com>
2025-07-25 01:44:59 +0600

    update
    toggle show Password and Accept Terms

--- email-password-auth/src/components/SignUp/SignUp.jsx
+++ email-password-auth/src/components/SignUp/SignUp.jsx
@@ -1,44 +1,52 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../firebase.init";
 import { useState } from "react";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+
 
 
 const SignUp = () => {
     const [success, setSuccess] = useState(false)
     const [errorMessage, setErrorMessage] = useState('');
+    const [showPassword, setShowPassword] = useState(false);
 
-    const handleSignUp = e =>{
+    const handleSignUp = e => {
         e.preventDefault();
         const email = e.target.email.value;
         const password = e.target.password.value;
-        console.log(email,password);
+        const terms = e.target.terms.checked;
+        console.log(email, password);
 
         // reset error and status
         setErrorMessage('');
         setSuccess(false);
+        if(!terms){
+            setErrorMessage('Please accept our terms and condition.');
+            return;
+        }
 
-        if(password.length < 6){
+        if (password.length < 6) {
             setErrorMessage('Password should be 6 character or longer');
             return;
         }
         const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&#^()\-_=+{}[\]|;:'",.<>\/?\\])[A-Za-z\d@$!%*?&#^()\-_=+{}[\]|;:'",.<>\/?\\]{8,}$/;
-        if(!passwordRegex.test(password)){
+        if (!passwordRegex.test(password)) {
             setErrorMessage('At least one uppercase, one lowercase, one number, one special character');
             return;
         }
 
         createUserWithEmailAndPassword(auth, email, password)
-        .then(result =>{
-            // console.log(result.user);
-            setSuccess(true);
-            
-        })
-        .catch(error => {
-            // console.log('ERROR', error.message);
-            setErrorMessage(error.message);
-            setSuccess(false);
-        })
-        
+            .then(result => {
+                // console.log(result.user);
+                setSuccess(true);
+
+            })
+            .catch(error => {
+                // console.log('ERROR', error.message);
+                setErrorMessage(error.message);
+                setSuccess(false);
+            })
+
     }
     return (
         <div className='max-w-lg mx-auto'>
@@ -51,11 +59,32 @@ const SignUp = () => {
                             <label className="label">Email</label>
                             <input type="email" name='email' className="input" placeholder="Email" />
 
-                            <label className="label">Password</label>
-                            <input type="password" name='password' className="input" placeholder="Password" />
+                            <div className="relative">
+                                <label className="label">Password</label>
+                                <input
+                                    type={showPassword ? "text" : "password"}
+                                    name='password'
+                                    className="input"
+                                    placeholder="Password" required />
+                                <button
+                                    onClick={() => setShowPassword(!showPassword)}
+                                    className="btn btn-xs absolute right-2 top-6">
+                                    {showPassword ? <FaEyeSlash /> : <FaEye />}
+                                </button>
 
-                            <div><a className="link link-hover">Forgot password?</a></div>
-                            <button className="btn btn-neutral mt-4">Login</button>
+                                <div><a className="link link-hover">Forgot password?</a></div>
+                                <div>
+                                    <fieldset className="fieldset bg-base-100 border-base-300 rounded-box w-64 border p-4">
+                                   
+                                        <label className="label">
+                                            <input type="checkbox" className="checkbox" name='terms' />
+                                            Accept Terms and Conditions.
+                                        </label>
+                                    </fieldset>
+                                </div>
+
+                                <button className="btn btn-neutral mt-4">Login</button>
+                            </div>
                         </fieldset>
                     </div>
                 </div>
