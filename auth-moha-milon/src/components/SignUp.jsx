f073c341ee5305a4f140e8fc2a963836fe8e671a
DASH-SIMANTA <cse.simanta.dash@gmail.com>
2025-07-28 02:46:44 +0600

    update
    create login and registration form

---
+++ auth-moha-milon/src/components/SignUp.jsx
@@ -0,0 +1,52 @@
+import { Link } from 'react-router-dom';
+
+const SignUp = () => {
+    const handleSignUp = e => {
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        const name = e.target.name.value;
+        console.log(email, password, name);
+    }
+    return (
+        <div className="hero bg-base-200 min-h-screen">
+            <div className="hero-content flex-col">
+                <div className="text-center lg:text-left">
+                    <h1 className="text-5xl font-bold">Sign up now!</h1>
+
+                </div>
+                <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                    <form onSubmit={handleSignUp} className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input type="text" name='name' placeholder="name" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" name='email' placeholder="email" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" name='password' placeholder="password" className="input input-bordered" required />
+                            
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">Sign up</button>
+                        </div>
+                    </form>
+                    <p className='ml-4 mb-4 mr-4'>
+                        Already have an account? please <Link to='/login'>Log in</Link>
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SignUp;
